6b0c9639369346d0417b8020ec7a00dcdd941629
Kaviarasan K K <kaviarasankk@gmail.com>
2017-06-26 16:54:48 +0530

    first commit

---
+++ kk_ga.php
@@ -0,0 +1,250 @@
+<?php
+/**
+* 2007-2015 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2015 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Kk_ga extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'kk_ga';
+        $this->tab = 'analytics_stats';
+        $this->version = '1.0.0';
+        $this->author = 'Kaviarasan K K';
+        $this->need_instance = 1;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l(' Google Analytics - EE');
+        $this->description = $this->l(' Google Analytics - Enhanced Ecommerce');
+        $this->confirmUninstall = $this->l('Are you sure want to uninstall google analytics module');
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        Configuration::updateValue('KK_GA_LIVE_MODE', false);
+        Configuration::updateValue('KK_GA_ECOMMERCE', false);
+        Configuration::updateValue('KK_GA_ENHANCED_ECOMMERCE', false);
+
+        return parent::install() &&
+            $this->registerHook('header') &&
+            $this->registerHook('displayFooter');
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('KK_GA_LIVE_MODE');
+        Configuration::deleteByName('KK_GA_ECOMMERCE');
+        Configuration::deleteByName('KK_GA_ENHANCED_ECOMMERCE');
+
+        return parent::uninstall();
+    }
+
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+        /**
+         * If values have been submitted in the form, process.
+         */
+        if (((bool)Tools::isSubmit('submitKk_gaModule')) == true) {
+            $this->postProcess();
+        }
+
+        // $output = $this->fetch('module:kk_ga/views/templates/admin/configure.tpl');
+
+        return $this->renderForm();
+    }
+
+    /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitKk_gaModule';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                'title' => $this->l('Settings'),
+                'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->l('Live mode'),
+                        'name' => 'KK_GA_LIVE_MODE',
+                        'is_bool' => true,
+                        'desc' => $this->l('Use this module in live mode'),
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => true,
+                                'label' => $this->l('Enabled')
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => false,
+                                'label' => $this->l('Disabled')
+                            )
+                        ),
+                    ),
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->l('Google Analytics Ecommerce'),
+                        'name' => 'KK_GA_ECOMMERCE',
+                        'is_bool' => true,
+                        'desc' => $this->l('Google Analytics Ecommerce'),
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => true,
+                                'label' => $this->l('Enabled')
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => false,
+                                'label' => $this->l('Disabled')
+                            )
+                        ),
+                    ),
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->l('Google Analytics Enhanced Ecommerce'),
+                        'name' => 'KK_GA_ENHANCED_ECOMMERCE',
+                        'is_bool' => true,
+                        'desc' => $this->l('Google Analytics Enhanced Ecommerce'),
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => true,
+                                'label' => $this->l('Enabled')
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => false,
+                                'label' => $this->l('Disabled')
+                            )
+                        ),
+                    ),
+                    array(
+                        'col' => 3,
+                        'type' => 'text',
+                        'prefix' => '<i class="icon icon-envelope"></i>',
+                        'desc' => $this->l('Enter google analytics tracking id'),
+                        'name' => 'KK_GA_TRACKING_ID',
+                        'label' => $this->l('TRAcking ID'),
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Set values for the inputs.
+     */
+    protected function getConfigFormValues()
+    {
+        return array(
+            'KK_GA_LIVE_MODE' => Configuration::get('KK_GA_LIVE_MODE', true),
+            'KK_GA_ECOMMERCE' => Configuration::get('KK_GA_ECOMMERCE', false),
+            'KK_GA_ENHANCED_ECOMMERCE' => Configuration::get('KK_GA_ENHANCED_ECOMMERCE', false),
+            'KK_GA_TRACKING_ID' => Configuration::get('KK_GA_TRACKING_ID', null),
+
+        );
+    }
+
+    /**
+     * Save form data.
+     */
+    protected function postProcess()
+    {
+        $form_values = $this->getConfigFormValues();
+
+        foreach (array_keys($form_values) as $key) {
+            Configuration::updateValue($key, Tools::getValue($key));
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path.'/views/js/ga.js');
+        $this->context->controller->addCSS($this->_path.'/views/css/ga.css');
+    }
+
+    public function hookDisplayFooter()
+    {
+        $this->context->controller->addJS($this->_path.'/views/js/ga.js');
+        $this->context->controller->addCSS($this->_path.'/views/css/ga.css');
+    }
+}
